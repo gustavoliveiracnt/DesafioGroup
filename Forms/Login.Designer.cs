fa65c674d196ea89248a1bf6498690bfe0e78489
Gustavo Oliveira <ooliveira.gustaavo@gmail.com>
2024-05-29 18:07:18 -0300

    formulario de login

---
+++ Forms/Login.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Desafio_Group
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.ImagemLogin = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.User = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ShowPass = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.EsqueciSenha = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagemLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImagemLogin
+            // 
+            this.ImagemLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ImagemLogin.Image = global::Desafio_Group.Properties.Resources.group;
+            this.ImagemLogin.Location = new System.Drawing.Point(12, 30);
+            this.ImagemLogin.Name = "ImagemLogin";
+            this.ImagemLogin.Size = new System.Drawing.Size(610, 85);
+            this.ImagemLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImagemLogin.TabIndex = 0;
+            this.ImagemLogin.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(148, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bem-vindos ao Group Cad!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.textBox1.Location = new System.Drawing.Point(153, 221);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(346, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // User
+            // 
+            this.User.AutoSize = true;
+            this.User.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.User.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.User.Location = new System.Drawing.Point(148, 189);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(110, 26);
+            this.User.TabIndex = 3;
+            this.User.Text = "Usuário:";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.Password.Location = new System.Drawing.Point(148, 254);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(94, 26);
+            this.Password.TabIndex = 5;
+            this.Password.Text = "Senha:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.textBox2.Location = new System.Drawing.Point(153, 286);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(174, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // ShowPass
+            // 
+            this.ShowPass.AutoSize = true;
+            this.ShowPass.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowPass.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.ShowPass.Location = new System.Drawing.Point(342, 282);
+            this.ShowPass.Name = "ShowPass";
+            this.ShowPass.Size = new System.Drawing.Size(148, 23);
+            this.ShowPass.TabIndex = 6;
+            this.ShowPass.Text = "Mostrar senha";
+            this.ShowPass.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Azure;
+            this.button1.Location = new System.Drawing.Point(153, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(346, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // EsqueciSenha
+            // 
+            this.EsqueciSenha.AutoSize = true;
+            this.EsqueciSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.EsqueciSenha.Location = new System.Drawing.Point(272, 362);
+            this.EsqueciSenha.Name = "EsqueciSenha";
+            this.EsqueciSenha.Size = new System.Drawing.Size(107, 16);
+            this.EsqueciSenha.TabIndex = 8;
+            this.EsqueciSenha.TabStop = true;
+            this.EsqueciSenha.Text = "Esqueci a senha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(169, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(312, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Insira dados abaixo para acessar o programa";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(634, 411);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EsqueciSenha);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ShowPass);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.User);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ImagemLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagemLogin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImagemLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label User;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox ShowPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel EsqueciSenha;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
--- Forms/Login.cs
+++ Forms/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Desafio_Group
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
